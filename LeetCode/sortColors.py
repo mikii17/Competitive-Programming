8ec78a030557c8e159d0ab859e89047c0f703d8c
mikii17 <michael12gashaw@gmail.com>
2022-10-05 08:01:17 -0700

    Leetcode Sort Colors

--- LeetCode/sortColors.py
+++ LeetCode/sortColors.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         index = 0
-        for i in range(3):
+        for i in range(2):
             for j in range(len(nums)):
                 if nums[j] == i:
                     nums[index], nums[j] = nums[j], nums[index]
